6773b593b101fd84db3edd2351f811ede45b8e14
Adam Jackson <ajackson2681@gmail.com>
2026-06-21 11:39:49 -0500

    Add board rev-specific defines

--- include/Config.hpp
+++ include/Config.hpp
@@ -2,6 +2,10 @@
 
 #include <string>
 
+#define BOARD_REV 0
+
+#if BOARD_REV <= 1
+
 #define RS_PIN 21
 
 #define D7_PIN 13
@@ -12,7 +16,19 @@
 #define E1_PIN 22
 #define E2_PIN 20
 
-#define CD_PIN 26
+#else if BOARD_REV == 2
+
+#define RS_PIN 9
+
+#define D7_PIN 13
+#define D6_PIN 14
+#define D5_PIN 12
+#define D4_PIN 15
+
+#define E1_PIN 10
+#define E2_PIN 8
+
+#endif
 
 #define COL_COUNT 40
 #define ROW_COUNT 4
