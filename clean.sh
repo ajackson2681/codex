c68230238e48fd387d4b8b7be65b6e0d0d7e2648
Adam Jackson <ajackson2681@gmail.com>
2026-06-20 19:26:45 -0500

    Add a script for removing all build artifacts

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# just removes all build artifacts
+rm -rf build/ CMakeFiles/ generated/ output/ pico-sdk/ cmake_install.cmake CMakeCache.txt compile_commands.json Makefile pico_flash_region.ld texteditor.uf2 
